71d77ff930c71bdd46e6c49e9691441ebcee2fe9
trangmai10 <trangmai14102002.nd@gmail.com>
2024-06-21 15:18:57 +0700

    Final commit

---
+++ Models/MTTT651Student.cs
@@ -0,0 +1,6 @@
+namespace MTTT651_baithilai;
+public class MTTT651Student {
+    public int StudentID { get; set; }
+    public string FullName { get; set;}
+    public DateTime NgaySinh{get; set; }
+    }
