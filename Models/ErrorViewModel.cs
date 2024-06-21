d005552a3b9dea8161164fe67aa45c6ad002f242
trangmai10 <trangmai14102002.nd@gmail.com>
2024-06-21 12:43:39 +0700

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MTTT_baithilai.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
